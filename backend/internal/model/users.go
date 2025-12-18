3cc70f2af13d91a0a379b7278c3d0da1ead683b7
Achmad Aulia Irsyad <achmadauliairsyad@gmail.com>
2025-12-18 23:27:53 +0800

    ubah json untuk passwordHash

--- backend/internal/model/users.go
+++ backend/internal/model/users.go
@@ -6,6 +6,6 @@ type User struct {
 	ID		string     `json:"id"`
 	Name	string     `json:"name"`
 	Email	string     `json:"email"`
-	PasswordHash string    `json:"password_hash"`
+	PasswordHash string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 }
