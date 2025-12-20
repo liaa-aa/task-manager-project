9155238d9bb9f3393c2725e6a03370122a95f019
Reyqal <akureyqal@gmail.com>
2025-12-20 09:35:46 +0800

    baatur route handlers dan ba pasang middleware

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,11 +6,12 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"github.com/liaa-aa/task-manager-project/backend/internal/database"
 	"github.com/liaa-aa/task-manager-project/backend/internal/handler"
-	"github.com/liaa-aa/task-manager-project/backend/internal/middleware"
+	customMiddleware "github.com/liaa-aa/task-manager-project/backend/internal/middleware"
 	"github.com/liaa-aa/task-manager-project/backend/internal/repository"
 	"github.com/liaa-aa/task-manager-project/backend/internal/service"
 )
@@ -39,24 +40,33 @@ func main() {
 
 	r := chi.NewRouter()
 
+	r.Use(middleware.Logger)   
+    r.Use(middleware.Recoverer)
+
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:5173"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 	}))
+	
+	r.Group(func(r chi.Router) {
+		r.Post("/register", authHandler.Register)
+		r.Post("/login", authHandler.Login)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(customMiddleware.AuthMiddleware) 
+		r.Get("/users", authHandler.GetAllUsers)
+	})
 
 	r.Route("/task", func (r chi.Router)  {
-		r.Use(middleware.AuthMiddleware)
+		r.Use(customMiddleware.AuthMiddleware)
 		r.Get("/", taskHandler.List)
 		r.Post("/", taskHandler.Create)
 		r.Get("/{id}", taskHandler.Get)
 		r.Put("/{id}", taskHandler.Update)
 		r.Delete("/{id}", taskHandler.Delete)
 	})
-
-	r.Post("/register", authHandler.Register)
-	r.Post("/login", authHandler.Login)
-	r.Get("/users", authHandler.GetAllUsers)
 
 	port := os.Getenv("PORT")
 	if port == "" {
