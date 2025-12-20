0597e1fdc481431a848a137a3645389496b88320
Reyqal <akureyqal@gmail.com>
2025-12-20 17:45:04 +0800

    mindahin routing di main.go ke routes/routes.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,15 +5,9 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"github.com/liaa-aa/task-manager-project/backend/internal/database"
-	"github.com/liaa-aa/task-manager-project/backend/internal/handler"
-	customMiddleware "github.com/liaa-aa/task-manager-project/backend/internal/middleware"
-	"github.com/liaa-aa/task-manager-project/backend/internal/repository"
-	"github.com/liaa-aa/task-manager-project/backend/internal/service"
+	"github.com/liaa-aa/task-manager-project/backend/internal/routes"
 )
 
 func main() {
@@ -25,48 +19,8 @@ func main() {
 	defer db.Close()
 
 	log.Println("database connected")
-	
-	userRepo := repository.NewUserRepositoryPostgres(db)
-	authService := service.NewAuthService(userRepo)
-	authHandler := handler.NewAuthHandler(authService)
 
-	categoryRepo := repository.NewCategoryRepository(db)
-	statusPrioritiesRepo := repository.NewStatusPrioritiesRepositoryPostgres(db)
-	taskRepo := repository.NewTaskRepository(db)
-	
-	taskService := service.NewTaskService(taskRepo, statusPrioritiesRepo, categoryRepo)
-
-	taskHandler := handler.NewTaskHandler(taskService)
-
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)   
-    r.Use(middleware.Recoverer)
-
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-	}))
-	
-	r.Group(func(r chi.Router) {
-		r.Post("/register", authHandler.Register)
-		r.Post("/login", authHandler.Login)
-	})
-
-	r.Group(func(r chi.Router) {
-		r.Use(customMiddleware.AuthMiddleware) 
-		r.Get("/users", authHandler.GetAllUsers)
-	})
-
-	r.Route("/task", func (r chi.Router)  {
-		r.Use(customMiddleware.AuthMiddleware)
-		r.Get("/", taskHandler.List)
-		r.Post("/", taskHandler.Create)
-		r.Get("/{id}", taskHandler.Get)
-		r.Put("/{id}", taskHandler.Update)
-		r.Delete("/{id}", taskHandler.Delete)
-	})
+	r := routes.SetupRoutes(db)
 
 	port := os.Getenv("PORT")
 	if port == "" {
---
+++ backend/internal/routes/routes.go
@@ -0,0 +1,57 @@
+package routes
+
+import (
+	"database/sql"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/liaa-aa/task-manager-project/backend/internal/handler"
+	customMiddleware "github.com/liaa-aa/task-manager-project/backend/internal/middleware"
+	"github.com/liaa-aa/task-manager-project/backend/internal/repository"
+	"github.com/liaa-aa/task-manager-project/backend/internal/service"
+)
+
+func SetupRoutes(db *sql.DB) *chi.Mux {
+	userRepo := repository.NewUserRepositoryPostgres(db)
+	categoryRepo := repository.NewCategoryRepository(db)
+	statusPrioritiesRepo := repository.NewStatusPrioritiesRepositoryPostgres(db)
+	taskRepo := repository.NewTaskRepository(db)
+
+	authService := service.NewAuthService(userRepo)
+	taskService := service.NewTaskService(taskRepo, statusPrioritiesRepo, categoryRepo)
+
+	authHandler := handler.NewAuthHandler(authService)
+	taskHandler := handler.NewTaskHandler(taskService)
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	}))
+
+	r.Group(func(r chi.Router) {
+		r.Post("/register", authHandler.Register)
+		r.Post("/login", authHandler.Login)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(customMiddleware.AuthMiddleware)
+		r.Get("/users", authHandler.GetAllUsers)
+	})
+
+	r.Route("/task", func(r chi.Router) {
+		r.Use(customMiddleware.AuthMiddleware)
+		r.Get("/", taskHandler.List)
+		r.Post("/", taskHandler.Create)
+		r.Get("/{id}", taskHandler.Get)
+		r.Put("/{id}", taskHandler.Update)
+		r.Delete("/{id}", taskHandler.Delete)
+	})
+
+	return r
+}
